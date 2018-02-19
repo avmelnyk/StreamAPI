2b1b01c2e911218a7999cf13e072a8c1635c02a8
avmelnyk <melnyk.andreew@gmail.com>
2018-02-19 15:15:14 +0200

    add db connection

--- controllers/index.php
+++ controllers/index.php
@@ -5,13 +5,26 @@ include("template.php");
 class IndexController {
 	
 	function get() {
-				$template = new Template("tmpl/");
-				$menu = array();
-				$menu["http://site.ru"] = "Главная";
-				$menu["http://site.ru/page-1.html"] = "Страница 1";
-				$menu["http://site.ru/page-2.html"] = "Страница 2";
-				$template->set("menu", $menu);
-				return $template->display("menu");
+		$template = new Template("tmpl/");
+		$menu = array();
+		$menu["http://site.ru"] = "Главная";
+		$menu["http://site.ru/page-1.html"] = "Страница 1";
+		$menu["http://site.ru/page-2.html"] = "Страница 2";
+		$template->set("menu", $menu);
+		return $template->display("menu");
+		
+		$user = "streaming";
+		$pass = "nyCjjHUUDO";
+		try {
+			$dbh = new PDO('pgsql:host=localhost; 	dbname=streaming', $user, $pass);
+			foreach($dbh->query('SELECT * from movies') as $row) {
+				print_r($row);
+			}
+			$dbh = null;
+		} catch (PDOException $e) {
+			print "Error!: " . $e->getMessage() . "<br/>";
+			die();
+		}
 	}
 	
 	function create () {
