caa62dff06876260c91e20f2144a0a38a3c8754e
avmelnyk <melnyk.andreew@gmail.com>
2018-02-19 16:59:08 +0200

    change db conf

--- controllers/index.php
+++ controllers/index.php
@@ -5,18 +5,19 @@ include("template.php");
 class IndexController {
 	
 	function get() {
-		$template = new Template("tmpl/");
-		$menu = array();
-		$menu["http://site.ru"] = "Главная";
-		$menu["http://site.ru/page-1.html"] = "Страница 1";
-		$menu["http://site.ru/page-2.html"] = "Страница 2";
-		$template->set("menu", $menu);
-		return $template->display("menu");
+		//~ $template = new Template("tmpl/");
+		//~ $menu = array();
+		//~ $menu["http://site.ru"] = "Главная";
+		//~ $menu["http://site.ru/page-1.html"] = "Страница 1";
+		//~ $menu["http://site.ru/page-2.html"] = "Страница 2";
+		//~ $template->set("menu", $menu);
+		//~ return $template->display("menu");
 		
 		$user = "streaming";
 		$pass = "nyCjjHUUDO";
+		
 		try {
-			$dbh = new PDO('pgsql:host=localhost; 	dbname=streaming', $user, $pass);
+			$dbh = new PDO('pgsql:host=localhost; dbname=streaming', $user, $pass);
 			foreach($dbh->query('SELECT * from movies') as $row) {
 				print_r($row);
 			}
