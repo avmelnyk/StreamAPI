61fa278fdf04d032b7952aa7a99c8efe04d835de
avmelnyk <melnyk.andreew@gmail.com>
2018-02-19 17:10:38 +0200

    add menu block

--- controllers/index.php
+++ controllers/index.php
@@ -5,19 +5,19 @@ include("template.php");
 class IndexController {
 	
 	function get() {
-		//~ $template = new Template("tmpl/");
-		//~ $menu = array();
-		//~ $menu["http://site.ru"] = "Главная";
-		//~ $menu["http://site.ru/page-1.html"] = "Страница 1";
-		//~ $menu["http://site.ru/page-2.html"] = "Страница 2";
-		//~ $template->set("menu", $menu);
-		//~ return $template->display("menu");
+		$template = new Template("tmpl/");
+		$menu = array();
+		$menu["http://site.ru"] = "Главная";
+		$menu["http://site.ru/page-1.html"] = "Страница 1";
+		$menu["http://site.ru/page-2.html"] = "Страница 2";
+		$template->set("menu", $menu);
+		$template->display("menu");
 		
 		$user = "streaming";
-		$pass = "nyCjjHUUDO";
+		$password = "nyCjjHUUDO";
 		
 		try {
-			$dbh = new PDO('pgsql:host=localhost; dbname=streaming', $user, $pass);
+			$dbh = new PDO('pgsql:host=localhost; dbname=streaming', $user, $password);
 			foreach($dbh->query('SELECT * from movies') as $row) {
 				print_r($row);
 			}
