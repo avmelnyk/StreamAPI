a887d55c1af8dd736a15bd6e2e799f4ed3b57e33
avmelnyk <melnyk.andreew@gmail.com>
2018-02-20 18:12:23 +0200

    change class

--- passwordreader.php
+++ passwordreader.php
@@ -2,7 +2,7 @@
 
 class PasswordReader {
 	
-	function readPaswword ($location) {
+	function readPassword ($location) {
 		$file_content = file_get_contents($location);
 		$password_json = json_decode($file_content, true); 
 		$password = $password_json["password"];
